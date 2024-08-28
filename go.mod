f4b62809bfc0c3b2733e5fd4ab5d22c30287c850
Jun Ohtani <johtani@gmail.com>
2024-08-28 23:47:40 +0900

    Upgrade kagome v2!

--- go.mod
+++ go.mod
@@ -4,6 +4,10 @@ go 1.21
 
 require (
 	github.com/go-ego/gse v0.80.2 // indirect
-	github.com/ikawaha/kagome v1.11.2 // indirect
+	github.com/ikawaha/kagome-dict v1.1.0 // indirect
+	github.com/ikawaha/kagome-dict-ipa-neologd v0.3.2 // indirect
+	github.com/ikawaha/kagome-dict/ipa v1.2.0 // indirect
+	github.com/ikawaha/kagome-dict/uni v1.2.0 // indirect
+	github.com/ikawaha/kagome/v2 v2.10.0 // indirect
 	github.com/vcaesar/cedar v0.20.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/go-ego/gse"
-	"github.com/ikawaha/kagome/tokenizer"
+	"github.com/ikawaha/kagome-dict/dict"
+	"github.com/ikawaha/kagome-dict/ipa"
+	"github.com/ikawaha/kagome-dict/uni"
+	"github.com/ikawaha/kagome/v2/tokenizer"
 )
 
 func main() {
@@ -14,9 +17,27 @@ func main() {
 		"これは世間を憚かる遠慮というよりも、その方が私にとって自然だからである。",
 		"素早い茶色の狐が怠けた犬を飛び越えた",
 		"すばやいちゃいろのきつねがなまけたいぬをとびこえた",
+		"きつね",
+		"関西国際空港は、日本の主要な国際空港の一つで、多くの外国人観光客が利用しています。",
+		"関西国際空港",
+		"隣の客は何をよく食べますか？",
+		"客",
+		`春の夜の夢はうつつよりもかなしき
+	夏の夜の夢はうつつに似たり
+	秋の夜の夢はうつつを超え
+	冬の夜の夢は心に響く
+
+	山のあなたに小さな村が見える
+	川の音が静かに耳に届く
+	風が木々を通り抜ける音
+	星空の下、すべてが平和である`,
+		"素早い茶色の狐が怠けた犬を飛び越えた",
+		"すばやいちゃいろのきつねがなまけたいぬをとびこえた",
+		"スバヤイチャイロノキツネガナマケタイヌヲトビコエタ",
+		"The quick brown fox jumps over the lazy dog",
 	}
 	var seg gse.Segmenter
-	err := seg.LoadDict("jp")
+	err := seg.LoadDict("ja")
 	if err != nil {
 		panic(err)
 	}
@@ -30,11 +51,11 @@ func main() {
 		println("------- GSE cut search --------")
 		tokenizeByGseCutSearch(text, seg)
 		println("------- Kagome ipa mode=Normal --------")
-		tokenizeByKagome(text, tokenizer.SysDicIPA(), tokenizer.Normal)
+		tokenizeByKagome(text, ipa.Dict(), tokenizer.Normal)
 		println("------- Kagome ipa mode=Search --------")
-		tokenizeByKagome(text, tokenizer.SysDicIPA(), tokenizer.Search)
+		tokenizeByKagome(text, ipa.Dict(), tokenizer.Search)
 		println("------- Kagome uni mode=Normal --------")
-		tokenizeByKagome(text, tokenizer.SysDicUni(), tokenizer.Normal)
+		tokenizeByKagome(text, uni.Dict(), tokenizer.Normal)
 	}
 
 }
@@ -51,8 +72,11 @@ func tokenizeByGseCutSearch(text string, seg gse.Segmenter) {
 	fmt.Println(seg.CutSearch(text))
 }
 
-func tokenizeByKagome(text string, dict tokenizer.Dic, mode tokenizer.TokenizeMode) {
-	t := tokenizer.NewWithDic(dict)
+func tokenizeByKagome(text string, dict *dict.Dict, mode tokenizer.TokenizeMode) {
+	t, err := tokenizer.New(dict)
+	if err != nil {
+		panic(err)
+	}
 	tokens := t.Analyze(text, mode)
 	print("[")
 	for _, token := range tokens {
